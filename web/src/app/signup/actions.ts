25daca7c1c7960df423909a6b0d485def5a677c9
Kushal Patel <kushal.patel3121@gmail.com>
2025-11-04 12:05:06 -0500

    Added signin and signup actions

---
+++ web/src/app/signup/actions.ts
@@ -0,0 +1,42 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server' 
+
+
+export async function signup(formData: any) {
+  const supabase = await createClient()
+  const form_data = {
+    email: formData?.email as string,
+    password: formData?.password as string,
+  }
+
+  // const {data, error} = await supabase.from('User').select('*').eq('email', form_data.email).maybeSingle();
+  // if (data) {
+  //   console.log("data: ", data);
+  //   return { error: new Error('User with this email already exists') }
+  // }
+  // if (error){
+  //   console.log("error: ", error);
+  //   return { error }
+  // }
+
+  const { error: signupError } = await supabase.auth.signUp(form_data)
+  if (signupError) {
+    return { error: signupError }
+  } 
+  else {
+    const { error: insertError } = await supabase.from('User').insert([{ 
+      email: formData.email as string,
+      username: formData.username as string,
+    }])
+    if (insertError) {
+      return { error: insertError }
+    }
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/signin')
+}
