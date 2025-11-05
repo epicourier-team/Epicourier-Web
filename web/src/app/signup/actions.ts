eee1eea43ca1477547681220e7706810e27a5f36
Kushal Patel <kushal.patel3121@gmail.com>
2025-11-05 16:40:50 -0500

    Records update issue resolved

--- web/src/app/signup/actions.ts
+++ web/src/app/signup/actions.ts
@@ -1,13 +1,17 @@
-"use server";
+'use server'
 
-import { createClient } from "@/utils/supabase/server";
+import { revalidatePath } from 'next/cache'
+import { redirect } from 'next/navigation'
+
+import { createClient } from '@/utils/supabase/server' 
+
 
 export async function signup(formData: { email?: string; password?: string; username?: string }) {
-  const supabase = await createClient();
+  const supabase = await createClient()
   const form_data = {
     email: formData?.email as string,
     password: formData?.password as string,
-  };
+  }
 
   // const {data, error} = await supabase.from('User').select('*').eq('email', form_data.email).maybeSingle();
   // if (data) {
@@ -19,20 +23,24 @@ export async function signup(formData: { email?: string; password?: string; user
   //   return { error }
   // }
 
-  const { error: signupError } = await supabase.auth.signUp(form_data);
+  const { error: signupError } = await supabase.auth.signUp(form_data)
   if (signupError) {
-    return { error: signupError };
-  } else {
-    const { error: insertError } = await supabase.from("User").insert([
-      {
-        email: formData.email as string,
-        username: formData.username as string,
-      },
-    ]);
-    if (insertError) {
-      return { error: insertError };
+    return { error: signupError }
+  } 
+  else {
+    const {data:dbGetData, error: dbGetError} = await supabase.from('User').select('*').eq('email', form_data.email).maybeSingle();
+    if (dbGetError  || !dbGetData) {
+      return { error: dbGetError? dbGetError : new Error('Failed to get user data') }
+    }
+    console.log("dbGetData: ", dbGetData);
+    const { error: updateUserError } = await supabase.from('User').update({username: formData?.username as string}).eq('id', dbGetData.id)
+    if (updateUserError) {
+      console.log("insertError: ", updateUserError);
+      return { error: updateUserError }
     }
   }
+  return {success: true}
 
-  return { success: true };
+  // revalidatePath('/', 'layout')
+  // redirect('/signin')
 }
